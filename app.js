9dd1a042a0c2c8dce468abcddddf051aed1dd166
Kevin Henrich <kevinhenrich@icloud.com>
2022-08-31 21:46:53 +0200

    Added Form Variables and Basic Task Array

---
+++ app.js
@@ -0,0 +1,18 @@
+const addTask = document.querySelector("#addTask");
+const taskTitle = document.querySelector('#taskTitle');
+const taskCat = document.querySelector('#taskCategory');
+
+let taskArr = [
+  {
+    title: "Tidy up living room",
+    category: "❤️ Personal",
+  },
+  {
+    title: "Finishing Project Presentation",
+    category: "🕑  Work",
+  },
+  {
+    title: "Studying JavaScript & React",
+    category: "🎓 Learning",
+  },
+];
