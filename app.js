96cf4f6269a9471425278a50a337b6297360e6cc
Kevin Henrich <kevinhenrich@icloud.com>
2022-08-31 22:49:02 +0200

    added 'Add Task' function and implemented Task Cards with buttons

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const addTask = document.querySelector("#addTask");
-const taskTitle = document.querySelector('#taskTitle');
-const taskCat = document.querySelector('#taskCategory');
+const taskTitle = document.querySelector("#taskTitle");
+const taskCat = document.querySelector("#TaskCategory");
+const app = document.querySelector(".app");
 
 let taskArr = [
   {
@@ -16,3 +17,44 @@ let taskArr = [
     category: "🎓 Learning",
   },
 ];
+
+// Function to display Tasks in the App Container
+const showTasks = () => {
+  for (let i = 0; i < taskArr.length; i++) {
+    let taskCard = `
+            <div class="task-card">
+                <div class="task-card-left">
+                    <input type="textarea" value="${taskArr[i].title}" /disabled />
+                    <div class="card-lower">
+                        <p>${taskArr[i].category}</p>
+                        <button class="task-button edit-button"><i class="fa-solid fa-pencil"></i></button>
+                        <button class="task-button"><i class="fa-solid fa-trash-can"></i></button>
+                        <button class="task-button check-button"><i class="fa-solid fa-check"></i></button>
+                    </div>
+                </div>
+            </div>
+        `;
+    app.innerHTML += taskCard;
+  }
+};
+
+// Initialize the Array in the App
+showTasks();
+
+// Add new Task on Submit
+addTask.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  newTask = {
+    title: taskTitle.value,
+    category: taskCat.options[taskCat.selectedIndex].value,
+  };
+
+  taskArr.push(newTask);
+  showTasks();
+});
+
+// Enable Edit, Delete and Done function
+taskArr.forEach((el) => {
+  
+});
