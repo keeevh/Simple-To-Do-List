6c0b2132169308740ef59d4574e05128c99a1994
Kevin Henrich <kevinhenrich@icloud.com>
2022-09-01 19:25:53 +0200

    added function to edit tasks and make edit button dynamically

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ let taskArr = [
   {
     title: "Tidy up living room",
     category: "❤️ Personal",
+    enableEdit: false,
   },
   {
     title: "Finishing Project Presentation",
     category: "🕑  Work",
+    enableEdit: false,
   },
   {
     title: "Studying JavaScript & React",
     category: "🎓 Learning",
+    enableEdit: false,
   },
 ];
 
@@ -25,12 +28,20 @@ const showTasks = () => {
     let taskCard = `
             <div class="task-card">
                 <div class="task-card-left">
-                    <input type="textarea" value="${taskArr[i].title}" /disabled />
+                    <input class="taskTitle${[i]}" type="textarea" value="${
+      taskArr[i].title
+    }" /disabled />
                     <div class="card-lower">
                         <p>${taskArr[i].category}</p>
-                        <button class="task-button edit-button"><i class="fa-solid fa-pencil"></i></button>
-                        <button class="task-button"><i class="fa-solid fa-trash-can"></i></button>
-                        <button class="task-button check-button"><i class="fa-solid fa-check"></i></button>
+                        <button class="task-button editBtn${[
+                          i,
+                        ]}"><i class="fa-solid fa-pencil"></i></button>
+                        <button class="task-button deleteBtn${[
+                          i,
+                        ]}"><i class="fa-solid fa-trash-can"></i></button>
+                        <button class="task-button checkBtn${[
+                          i,
+                        ]}"><i class="fa-solid fa-check"></i></button>
                     </div>
                 </div>
             </div>
@@ -56,3 +67,29 @@ addTask.addEventListener("click", (event) => {
 });
 
 // Enable Edit, Delete and Done function
+for (let i = 0; i < taskArr.length; i++) {
+  // Edit Task
+  let getEditBtn = ".editBtn" + [i];
+  let editBtn = document.querySelector(getEditBtn);
+  let getTaskTitle = ".taskTitle" + [i];
+  let taskTitle = document.querySelector(getTaskTitle);
+
+  editBtn.addEventListener("click", () => {
+    if (taskArr[i].enableEdit === false) {
+      taskTitle.disabled = false;
+      taskTitle.focus();
+      taskArr[i].enableEdit = true;
+      editBtn.innerHTML = '<i class="fa-solid fa-floppy-disk"></i>';
+      editBtn.style.background = "rgb(241, 182, 22)";
+
+      editBtn.addEventListener("click", (e) => {
+        taskArr[i].title = taskTitle.value;
+      });
+    } else {
+      taskTitle.disabled = true;
+      taskArr[i].enableEdit = false;
+      editBtn.innerHTML = '<i class="fa-solid fa-pencil"></i>';
+      editBtn.style.background = "#303030";
+    }
+  });
+}
